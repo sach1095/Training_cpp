f3c1a4a368cb8c1bf546f4d6076eadc3d853efc8
Sacha Baranes <sbaranes@z1r2p5.42lyon.fr>
2021-10-28 18:33:15 +0200

    start project

---
+++ Project_1/Includes/my_lib.hpp
@@ -0,0 +1,8 @@
+#ifndef __MY_LIB_HPP_
+# define __MY_LIB_HPP_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#endif
---
+++ Project_1/main.cpp
@@ -0,0 +1,7 @@
+#include "Includes/my_lib.hpp"
+
+int main()
+{
+	std::cout << "Welcom to sacha's games" << std::endl;
+	return 0;
+}
